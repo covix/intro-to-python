1dccc2c46ed15f40e24d76d39abee8304ad1159c
Luca Coviello <luca.coviello@gmail.com>
2019-10-04 11:16:35 +0200

    First commit

---
+++ 1_variables_exercises.py
@@ -0,0 +1,19 @@
+# first name case
+first_name = "luca"
+
+print(first_name)
+print(first_name.upper())
+print(first_name.title())
+print()
+
+# order of operations
+std_ord_calc = 1 + 3 / 2
+my_ord_calc = (1 + 3) / 2
+
+print("1 + 3 / 2 is:,", std_ord_calc)
+print("(1 + 3) / 2 is:,", my_ord_calc)
+print()
+
+# long decimals
+print("look at the decimals of (0.1 + 0.2) * 2:", (0.1 + 0.2) * 2)
+print()
---
+++ README.md
@@ -0,0 +1,6 @@
+# Intro to Python course repository
+
+here you can find the solution exercises for the Python introduction course.
+
+Exercises are taken from http://introtopython.org/.
+The original repository for the course is https://github.com/ehmatthes/intro_programming
