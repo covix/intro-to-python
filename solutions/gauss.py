a984e1ff33b5125d07489f09463bb868151e313c
Luca Coviello <luca.coviello@gmail.com>
2019-10-04 19:03:08 +0200

    Added gauss solution without functions

---
+++ solutions/gauss.py
@@ -0,0 +1,12 @@
+s = 0
+n = 0
+l = list(range(1, 101))
+
+while len(l) > 1:
+    a, b = l.pop(0), l.pop()
+    s += a + b
+    n += 1
+
+    print(f"\t current sum is: {s}, just added {a} and {b}")
+
+print(f"there are {n} pairs and then total sum is: {s}")
