7fcc9d256d42eb34fa5b61f36f702b6a4ceb0238
Luca Coviello <luca.coviello@gmail.com>
2019-10-05 09:32:47 +0200

    Fixed numerical lists exercises

--- solutions/3_numerical_lists.py
+++ solutions/3_numerical_lists.py
@@ -1,10 +1,10 @@
 # multiples of ten
-tens = []
+tens = [i * 10 for i in range(1, 11)]
+print("Multiples of ten:", tens)
 
-for number in range(10, 101, 10):
-    tens.append(number)
-
-print(tens)
+# but also
+tens = list(range(10, 101, 10))
+print("Multiples of ten v2:", tens)
 
 # working backwards
 plus_thirteen = []
@@ -12,4 +12,4 @@ plus_thirteen = []
 for number in range(1, 11):
     plus_thirteen.append(number + 13)
 
-print(plus_thirteen)
+print("Working backwards:", plus_thirteen)
