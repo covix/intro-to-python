dc4449fe9c98ce21d73a10dd5ad5291da5d82c86
Luca Coviello <luca.coviello@gmail.com>
2019-10-07 15:41:17 +0200

    Added pdf slides

--- solutions/2_lists.py
+++ solutions/2_lists.py
@@ -1,56 +1,71 @@
 # working list
-careers = ['programmer', 'truck driver', 'astronaut', 'farmer']
+careers = ["programmer", "truck driver", "astronaut", "farmer"]
 print(careers)
 
-print('The index of "astronaut" is:', careers.index('astronaut'))
-print('Is "farmer" in the list?', 'farmer' in careers)
-print('Is "potato" in the list?', 'potato' in careers)
+print('The index of "astronaut" is:', careers.index("astronaut"))
+print('Is "farmer" in the list?', "farmer" in careers)
+print('Is "potato" in the list?', "potato" in careers)
 
-careers.append('cook')
+careers.append("cook")
 print(careers)
-careers.insert(0, 'scientist')
+careers.insert(0, "scientist")
 print(careers)
 
 for job in careers:
     print(job)
 
 # ordered working list
-careers = ['programmer', 'truck driver', 'astronaut', 'farmer']     # re-set list to original
+careers = [
+    "programmer",
+    "truck driver",
+    "astronaut",
+    "farmer",
+]  # re-set list to original
 
-print('This is the list in its original order:')
+print("This is the list in its original order:")
 for job in careers:
     print(job)
 
-print('This is the list in alphabetical order:')
+print()
+print("This is the list in alphabetical order:")
 for job in sorted(careers):
     print(job)
 
-print('This is the list in its original order:')
+print()
+print("This is the list in its original order:")
 for job in careers:
     print(job)
 
-print('This is the list in reverse alphabetical order:')
+print()
+print("This is the list in reverse alphabetical order:")
 for job in sorted(careers, reverse=True):
     print(job)
 
-print('This is the list in its original order:')
+print()
+print("This is the list in its original order:")
 for job in careers:
     print(job)
 
-print('This is the list in reverse order from what it started:')
+print()
+print("This is the list in reverse order from what it started:")
 for job in reversed(careers):
     print(job)
 
-print('This is the list in its original order:')
+print()
+print("This is the list in its original order:")
 for job in careers:
     print(job)
 
-print('This is the list permanently sorted in alphabetical order:')
+print()
+print("This is the list permanently sorted in alphabetical order:")
 careers.sort()
 for job in careers:
     print(job)
 
-print('This is the list permanently sorted in reverse alphabetical order:')
+print()
+print("This is the list permanently sorted in reverse alphabetical order:")
 careers.sort(reverse=True)
 for job in careers:
     print(job)
+
+print()
